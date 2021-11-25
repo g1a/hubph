410f981c376613ed098badcca39a4cfe746e35cc
Kevin Porras <kporras07@gmail.com>
2021-11-25 14:40:54 -0600

    Move find line logic to a separate function.

--- src/Cli/ExtraCommands.php
+++ src/Cli/ExtraCommands.php
@@ -80,7 +80,6 @@ class ExtraCommands extends \Robo\Tasks implements ConfigAwareInterface, LoggerA
         $baseBranch = $options['base-branch'];
         $workingCopy = WorkingCopy::cloneBranch($url, $dir, $baseBranch, $api);
 
-        // Set logger for workingCopy, is this correct?
         if ($this->logger) {
             $workingCopy->setLogger($this->logger);
         }
@@ -107,50 +106,16 @@ class ExtraCommands extends \Robo\Tasks implements ConfigAwareInterface, LoggerA
             else {
                 $readme_contents = '';
             }
+
             $lines = explode("\n", $readme_contents);
-            $number_of_lines_to_search = 5;
-            $first_empty_line = -1;
-            $last_badge_line = -1;
-            $badge_insert_line = -1;
-            foreach ($lines as $line_number => $line) {
-                if ($first_empty_line == -1 && empty(trim($line))) {
-                    $first_empty_line = $line_number;
-                }
-                // Is this line a badge?
-                if (preg_match('/\[\!\[[A-Za-z0-9\s]+\]\(.*\)/', $line)) {
-                    $last_badge_line = $line_number;
-                    // Is this line the License badge?
-                    if (preg_match('/\[\!\[License]\(.*\)/', $line)) {
-                        if ($line_number) {
-                            $badge_insert_line = $line_number - 1;
-                        } else {
-                            $badge_insert_line = 0;
-                        }
-                    }
-                } else {
-                    if ($last_badge_line != -1) {
-                        // We already found the badges, exit foreach.
-                        break;
-                    } elseif ($line_number > $number_of_lines_to_search) {
-                        // We've searched enough lines, exit foreach.
-                        break;
-                    }
-                }
-            }
-            if ($badge_insert_line === -1) {
-                if ($last_badge_line !== -1) {
-                    // If we found badges, we'll insert this badge after the last badge.
-                    $badge_insert_line = $last_badge_line + 1;
-                } elseif ($first_empty_line !== -1) {
-                    // If we didn't find any badges, we'll insert this badge at the first empty line.
-                    $badge_insert_line = $first_empty_line + 1;
-                } else {
-                    // Final fallback: insert badge in the second line of the file.
-                    $badge_insert_line = 1;
-                }
-            }
+            [$badge_insert_line, $empty_line_after] = $this->getBadgeInsertLine($lines);
+
             // Insert badge contents and empty line after it.
-            array_splice($lines, $badge_insert_line, 0, [$badge, '']);
+            $insert = [$badge];
+            if ($empty_line_after) {
+                $insert[] = '';
+            }
+            array_splice($lines, $badge_insert_line, 0, $insert);
             $readme_contents = implode("\n", $lines);
             file_put_contents("$dir/README.md", $readme_contents);
             $workingCopy->add("$dir/README.md");
@@ -162,6 +127,55 @@ class ExtraCommands extends \Robo\Tasks implements ConfigAwareInterface, LoggerA
         $message = $options['pr-title'];
         $body = $options['pr-body'];
         $workingCopy->pr($message, $body, $baseBranch, $branchName);
+    }
+
+    /**
+     * Get line number where to insert the badge.
+     */
+    protected function getBadgeInsertLine($readme_lines, $number_of_lines_to_search = 5) {
+        $first_empty_line = -1;
+        $last_badge_line = -1;
+        $badge_insert_line = -1;
+        $empty_line_after = true;
+        foreach ($readme_lines as $line_number => $line) {
+            if ($first_empty_line == -1 && empty(trim($line))) {
+                $first_empty_line = $line_number;
+            }
+            // Is this line a badge?
+            if (preg_match('/\[\!\[[A-Za-z0-9\s]+\]\(.*\)/', $line)) {
+                $last_badge_line = $line_number;
+                // Is this line the License badge?
+                if (preg_match('/\[\!\[License]\(.*\)/', $line)) {
+                    if ($line_number) {
+                        $badge_insert_line = $line_number;
+                    } else {
+                        $badge_insert_line = 0;
+                    }
+                    $empty_line_after = false;
+                }
+            } else {
+                if ($last_badge_line != -1) {
+                    // We already found the badges, exit foreach.
+                    break;
+                } elseif ($line_number > $number_of_lines_to_search) {
+                    // We've searched enough lines, exit foreach.
+                    break;
+                }
+            }
+        }
+        if ($badge_insert_line === -1) {
+            if ($last_badge_line !== -1) {
+                // If we found badges, we'll insert this badge after the last badge.
+                $badge_insert_line = $last_badge_line + 1;
+            } elseif ($first_empty_line !== -1) {
+                // If we didn't find any badges, we'll insert this badge at the first empty line.
+                $badge_insert_line = $first_empty_line + 1;
+            } else {
+                // Final fallback: insert badge in the second line of the file.
+                $badge_insert_line = 1;
+            }
+        }
+        return [$badge_insert_line, $empty_line_after];
     }
 
     /**
